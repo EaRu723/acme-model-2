e3b71d27519dd00769629518cb620230fe3d2ab5
EaRu723 <y.andrearusso@gmail.com>
2024-07-25 19:37:20 -0400

    added dates to db and storage, re-did copy, removed routine upload

--- streamlit_opt.py
+++ streamlit_opt.py
@@ -50,9 +50,12 @@ def load_model():
 
 def save_prediction(db, user_email, side, scores, img_url):
     user = user_email.replace("@", "_").replace(".", "_")
+    date_today = datetime.datetime.now().strftime("%Y-%m-%d")
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
     user_doc_ref = db.collection("webApp").document(user)
-    predictions_col_ref = user_doc_ref.collection("predictions")
-    predictions_col_ref.add({
+    predictions_col_ref = user_doc_ref.collection("predictions").document(date_today)
+    predictions_col_ref.collection("entries").document(f"{timestamp}_{side}").set({
         'side': side,
         'scores': {
             "severity_score": scores[0][0],
@@ -62,13 +65,15 @@ def save_prediction(db, user_email, side, scores, img_url):
         'img_url': img_url
     })
 
+
 def save_image(user_email, side, _img, img_name):
     user = user_email.replace("@", "_").replace(".", "_")
+    date_today = datetime.datetime.now().strftime("%Y-%m-%d")
     temp_file_path = f"temp_{side}.jpg"
     _img.save(temp_file_path, optimize=True, quality=85)
 
     bucket = storage.bucket()
-    blob = bucket.blob(f"webApp/{user}/{img_name}")
+    blob = bucket.blob(f"webApp/{user}/{date_today}/{img_name}")
 
     try:
         blob.upload_from_filename(temp_file_path)
@@ -77,7 +82,8 @@ def save_image(user_email, side, _img, img_name):
     finally:
         os.remove(temp_file_path)
 
-    return f"gs://{st.secrets['storageBucket']}/webApp/{user}/{img_name}"
+    return f"gs://{st.secrets['storageBucket']}/webApp/{user}/{date_today}/{img_name}"
+
 
 def process_image(image, side, email, model, database):
     if image:
@@ -208,11 +214,22 @@ def all_users(db, user_email):
 
 def store_routine(_db, routine, user_email):
     user = user_email.replace("@", "_").replace(".", "_")
+    date_today = datetime.datetime.now().strftime("%Y-%m-%d")
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
     user_doc_ref = _db.collection("webApp").document(user)
-    predictions_col_ref = user_doc_ref.collection("routines")
-    predictions_col_ref.add({
-        'routine': routine
+    routines_col_ref = user_doc_ref.collection("routines").document(date_today)
+    routines_col_ref.collection("entries").document(timestamp).set({
+        'routine': routine,
+        'date': datetime.datetime.now()
     })
+
+# Define the path to the images
+image_directory = "/Users/andrea/Documents/Streamlit/acme-model-2/images"
+
+# List of image file names
+image_files = ["IMG_1008.jpeg"]
+
 
 
 def main():
@@ -220,36 +237,33 @@ def main():
     database = init_firebase()
     page_visit_update(database)
 
-    st.title("Clear Your Skin")
-    st.write("find what works for ***you***")
-
-    st.subheader("How?")
+    st.title("Is Your Acne Improving?")
+    st.subheader("Hi 👋 I'm Andrea, and I'm crowdsourcing the cure to acne.")
     st.write("""
-    **Track:** Log your skincare, diet, and lifestyle habits  
-    **Evaluate:** Use our state-of-the-art AI to objectively measure progress  
-    **Iterate:** Receive personalized recommendations on what works and what doesn't
+    I've struggled with acne for years and though there are many "solutions" on the market, none have worked for me. 
+    I know this is a common experience, so I decided to tackle this problem myself. That's why I created this free tool, which uses AI to honestly assess
+    if your skin is improving.
+    It's based on the gold-standard [Hayashi scale](https://pubmed.ncbi.nlm.nih.gov/18477223/) and a [model](https://arxiv.org/abs/2403.00268) developed by researchers at MIT. **Try it out!**
     """)
-
-    st.subheader("Clear Skin Starts Here:")
-    st.write("**Upload photo(s)**: Show us your starting point")
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
         st.subheader("Left")
-        left_image = st.file_uploader("", type=["jpg", "jpeg", "png"], key="left")
+        left_image = st.file_uploader("(photo upload)", key="left")
 
     with col2:
         st.subheader("Front")
-        front_image = st.file_uploader("", type=["jpg", "jpeg", "png"], key="front")
+        front_image = st.file_uploader("(photo upload)", key="front")
 
     with col3:
         st.subheader("Right")
-        right_image = st.file_uploader("", type=["jpg", "jpeg", "png"], key="right")
+        right_image = st.file_uploader("(photo upload)", key="right")
 
-    routine = st.text_area("**Describe your routine**: products, frequency of use, diet, and any other relevant details", help="If you don't have a routine we have a great one below.")
-
-    email = st.text_input("**Enter your email**: (optional) get personal insights straight in your inbox", help="If you provide your email we'll keep track of your progress for you.")
+    # routine = st.text_area("**Share your routine**: products, frequency of use, diet, and any other relevant details", help="If you don't have a routine we have a great one below.")
+    
+    st.write("I've been using it to evaluate the efficacy of different protocols I'm experimenting with. The model helps to make sure I'm being objective. I've been tracking [my journey](https://www.instagram.com/andreas_acne_journey/) for months and I have a lot more planned if people like it. Sign up to my mailing list to keep in touch!")
+    email = st.text_input("**Enter your email**: (optional) be the first to know about new features and get personal insights straight in your inbox", help="If you provide your email we'll keep track of your progress for you.")
 
     if st.button("Submit"):
         if email and (not is_valid_email(email)):
@@ -281,15 +295,24 @@ def main():
                 st.success("Results have been emailed to you!")
 
 
+    st.header("Coming Soon…")
+    st.write("""
+    - Personalized Progress Reports
+    - Protocol assessment
+    - Supportive Community Access
+    - iOS App
+    """)
+
     st.subheader("About")
     st.write("""
-            We use the gold-standard Hayashi method to provide a dermatologist grade evaluation with two outputs per image: a classification into one of four distinct complexions (clear, mild, moderate, severe) and a detailed blemish count.    
+            The goal is to make sel-experimentation collaborative. By working together we can learn what works and what doesn't and put acne behind us for good.    
     """)
     st.write("If you have any questions or need support, feel free to reach out to us at [team@yacne.com](mailto:team@yacne.com).")
 
+
     st.subheader("Who We Are")
     st.write("""
-    I’m Andrea, a MS in Biomedical Engineering and a self-taught software developer passionate about health. After struggling with acne for years, I found this AI model and used it to clear my skin.I'm sharing what's worked for me in the hopes of helping others.
+    I’m Andrea, a MS in Biomedical Engineering and a self-taught software developer who is passionate about health. After struggling with acne for years, I found this AI model and used it to clear my skin.I'm sharing what's worked for me in the hopes of helping others.
     
     Find me on social media:
     - [LinkedIn](https://www.linkedin.com/in/earu723/)
@@ -297,7 +320,13 @@ def main():
     - [TikTok](https://www.tiktok.com/@y.earu)
     - [Instagram](https://www.instagram.com/y.earu/)
     - [Twitter](https://x.com/AndreaR91659141)
+    """)
+    
+    for image_file in image_files:
+        image_path = os.path.join(image_directory, image_file)
+        st.image(image_path, caption=os.path.splitext("me as a 4th year BME student")[0], use_column_width=True)
 
+    st.write("""
     I'm Dana, a BASc in Biomedical Engineering, specializing in AI. Andrea's story spoke to me and I wanted to help him share this model with others.
     You can find me [here](https://www.linkedin.com/in/danazarezankova/).
     """)
@@ -322,12 +351,6 @@ def main():
     - Practice meditation to reduce stress.
     """)
 
-    st.header("Coming Soon…")
-    st.write("""
-    - Personalized Progress Reports
-    - Supportive Community Access
-    - iOS App
-    """)
 
 if __name__ == "__main__":
     main()
